c1e7590eb132e0a5f0cbe9741a3fd471d530a8c8
patrizineversuxxx <r2tr@yandex.ru>
2018-03-24 19:23:01 +0400

    One step closer to the

--- Program.cs
+++ Program.cs
@@ -69,87 +69,107 @@ namespace SuperFilter
             int N = 2 * size + 1;
             
             Stopwatch stopwatch = Stopwatch.StartNew();
-            
-            Parallel.For(size, image.Width - size-1, x =>
-            {
-                for(int y=size; y<image.Height-size-1; y++)
+            int c = 5;//параметры фильтра
+            //for (double w = 0.1; w < 3; w += 0.05)
+            //{
+                //Parallel.For(size, image.Width - size-1, x =>
+                for (int x = size; x < image.Width - size - 1; x++)
                 {
-                    Color[,] colors = new Color[N,N];//массив цветов 
-                    int [,] r = new int[N,N];//массив радиусов
-                    double [,] h = new double[N,N];//массив импульсной характеристики пикселей окна
-                    double[] H = new double[size+1];//массив импульсной характеристики, зависящей от радиуса
-                    double[,,] resultH = new double[N,N,3];
-                    Color central = Color.Black;
-                    
-                    
-                    int left = x - size;
-                    int right = x + size;
-                    int up = y - size;
-                    int down = y + size;
-                    int n = 0;
-
-                    int c = 5;//параметры фильтра
-                    double w = 1;//параметры фильтра
-                    
-                    for (int i = left; i <= right; i++)
+                    for (int y = size; y < image.Height - size - 1; y++)
                     {
-                        for (int j = up; j <= down; j++)
+                        Color[,] colors = new Color[N, N]; //массив цветов 
+                        int[,] r = new int[N, N]; //массив радиусов
+                        double[,] h = new double[N, N]; //массив импульсной характеристики пикселей окна
+                        double[] H = new double[N]; //массив импульсной характеристики, зависящей от радиуса
+                        double resultH = 0;
+                        double[,,] convolution = new double[N, N, 3];
+                        Color central = Color.Black; //запись центрального отсчёта
+
+
+                        int left = x - size;
+                        int right = x + size;
+                        int up = y - size;
+                        int down = y + size;
+
+
+                        double w = 1.5;//параметры фильтра
+
+                        for (int i = left; i <= right; i++)
                         {
-                            int I = i - left, J = j - up;
-                            colors[I,J] = image.GetPixel(i, j);
-                            if (i==x && j==y)
+                            for (int j = up; j <= down; j++)
                             {
-                                central = colors[I,J];
+                                int I = i - left, J = j - up;
+                                colors[I, J] = image.GetPixel(i, j);
+                                if (i == x && j == y)
+                                {
+                                    central = colors[I, J];
+                                }
+
+                                r[I, J] = (int) Math.Sqrt(Math.Pow(I - (N - 1) / 2, 2) +
+                                                          Math.Pow(J - (N - 1) / 2, 2)); //вычисление радиуса
+
+
+                                H[r[I, J]] = Math.Exp(-c * w) / Math.PI *
+                                             (Math.Sin(w * r[I, J]) / r[I, J] +
+                                              2 * Math.Cos(w * r[I, J]) / (Math.Pow(r[I, J], 2) * w) +
+                                              2 * Math.Sin((w * r[I, J])) / (Math.Pow(r[I, J], 3) * Math.Pow(w, 2)) +
+                                              (c * Math.Cos(w * r[I, J]) - Math.Sin((w * r[I, J]))) /
+                                              (Math.Pow(c, 2) + Math.Pow(r[I, J], 2))); //вычисление импульсного отклика
+                                H[0] = 0;
+
+                                h[I, J] = H[r[I, J]]; //передача соответсвующих значений импусльного отклика
+                                resultH += h[I, J];
                             }
-                            
-                            r[I,J] = (int)Math.Sqrt(Math.Pow(I - (N - 1) / 2, 2) + Math.Pow(J - (N - 1) / 2, 2));
-                            
-                            
-                            H[r[I,J]] = Math.Exp(-c * w) / Math.PI *
-                                      (Math.Sin(w * r[I,J]) / r[I,J] +
-                                       2 * Math.Cos(w * r[I,J]) / (Math.Pow(r[I,J], 2) * w) +
-                                       2 * Math.Sin((w * r[I,J])) / (Math.Pow(r[I,J], 3) * Math.Pow(w, 2)) +
-                                       (c * Math.Cos(w * r[I,J]) - Math.Sin((w * r[I,J]))) /
-                                       (Math.Pow(c, 2) + Math.Pow(r[I,J], 2)));
-                            H[0] = 0;
-                            
-                            h[I, J] = H[r[I, J]];
-                            colors[I, J] = Color.FromArgb(
-                                255, 
-                                (colors[I,J].R*(int)h[I,J]),
-                                (colors[I,J].G*(int)h[I,J]),
-                                (colors[I,J].B*(int)h[I,J])
-                                );
-                            resultH[I,J,0] += colors[I,J].R;
-                            resultH[I,J,1] += colors[I,J].G;
-                            resultH[I,J,2] += colors[I,J].B;
+                        }
+
+                        double norm = 1 / (2 * resultH); //нормирование 
+                        //Console.WriteLine(norm);
+                        for (int i = left; i <= right; i++)
+                        {
+                            for (int j = up; j <= down; j++)
+                            {
+                                int I = i - left, J = j - up;
+                                h[I, J] *= norm;
+                            }
+                        }
+
+                        for (int X = left; X <= right; X++)
+                        {
+                            for (int Y = up; Y <= down; Y++)
+                            {
+                                double resultR = 0, resultG = 0, resultB = 0;
+                                //double resultR = central.R, resultG = 2*norm*central.G, resultB = 2*norm*central.B;
+                                for (int I = X - left; I < N; I++)
+                                {
+                                    for (int J = Y - up; J < N; J++)
+                                    {
+                                        resultR += colors[I, J].R * h[I, J];
+                                        resultG += colors[I, J].G * h[I, J];
+                                        resultB += colors[I, J].B * h[I, J];
+                                        Color result = Color.FromArgb(255, (int) resultR, (int) resultG, (int) resultB);
+                                        outBitmapLock.SetPixel(X, Y, result);
+                                    }
+                                }
+                                
+                            }
                         }
                     }
 
-                    resultH[x - left, y - up, 0] = 1 / (2 * resultH[x - left, y - up, 0]);
-                    resultH[x - left, y - up, 1] = 1 / (2 * resultH[x - left, y - up, 1]);
-                    resultH[x - left, y - up, 2] = 1 / (2 * resultH[x - left, y - up, 2]);
-                        //h[x-left, y-up] = 1 / (2 * keek);//исправить эту строчку
-                        Color result = Color.FromArgb(255,
-                            (int) resultH[x - left, y - up, 0] * central.R,
-                            (int) resultH[x - left, y - up, 1] * central.G,
-                            (int) resultH[x - left, y - up, 2] * central.B);
+                } //);
+            //}
 
-                        outBitmapLock.SetPixel(x, y, result);
-                }
-                
-            });
             stopwatch.Stop();
             Console.Write(stopwatch.ElapsedMilliseconds);
             return outBitmapLock.Release();
+            
         }
         
         public static void Main(string[] args)
         {
-            Bitmap im = (Bitmap) Image.FromFile("C:/IO/4b.png", false);
+            Bitmap im = (Bitmap) Image.FromFile("C:/IO/5b.png", false);
 
-//            MedianFilter(im,5).Save("C:/IO/out_ss.jpg");
-            SuperFilter(im,1).Save("C:/IO/diploma.jpg");
+            //MedianFilter(im,5).Save("C:/IO/out_ss.jpg");
+            SuperFilter(im,1).Save("C:/IO/diploma2.jpg");
         }
     }
 }
